c315f380684a8dcce28aec70af1571fd79e17da3
Niraj Modi <modiniraj1034@gmail.com>
2024-01-22 18:11:43 +0530

    PrimeReact Setup

--- src/components/common/Custom_Table/index.tsx
+++ src/components/common/Custom_Table/index.tsx
@@ -1,7 +1,19 @@
+"use client";
+import axios from "axios";
 import React from "react";
+import { DataTable } from "primereact/datatable";
 
 const Custom_Table: React.FC = () => {
-  return <div></div>;
+  const [data, setData] = React.useState([]);
+  React.useEffect(() => {
+    (async () => {
+      const response = await axios.get(
+        "https://api.npoint.io/b4e5dbdf2e9ad517f981"
+      );
+      setData(response.data);
+    })();
+  });
+  return <div>{JSON.stringify(data)}</div>;
 };
 
 export default Custom_Table;
