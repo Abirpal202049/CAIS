2dad49e453e1375065c194f1dce61aa4e962f0cd
mehraankush <mehraankush36@gmail.com>
2024-01-22 18:44:59 +0530

    added @svgr/webpack

---
+++ src/data/svgr/Filters/index.ts
@@ -0,0 +1,8 @@
+export { default as Group30 } from './Group30.svg'
+export { default as Group29 } from './Group29.svg'
+export { default as File04 } from './file04.svg'
+export { default as User02 } from './users02.svg'
+export { default as Potrait } from './potrait.svg'
+export { default as Search } from './search.svg'
+export { default as MenuCircleVertical } from './menu-circle-vertical.svg'
+export { default as Attachment } from './attachment.svg'
---
+++ src/data/svgr/navbar/index.ts
@@ -0,0 +1,4 @@
+export { default as HalfMoon } from './half moon.svg'
+export { default as Notification } from './notification.svg'
+export { default as Search } from './search.svg'
+export { default as Setting } from './setting.svg'
