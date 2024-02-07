9ccb86b470f534d8148ce87a14acfaf02ef18034
Niraj Modi <modiniraj1034@gmail.com>
2024-02-07 16:42:21 +0530

    build fix

--- src/components/common/Sidebar/OptionsComponent.tsx
+++ src/components/common/Sidebar/OptionsComponent.tsx
@@ -1,85 +1,103 @@
-"use client"
-import React, { Key, useRef } from 'react'
-import { usePathname } from 'next/navigation';
-import Link from 'next/link';
-import { Ripple } from 'primereact/Ripple';
-import { StyleClass } from 'primereact/StyleClass';
+"use client";
+import React, { Key, useRef } from "react";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import { Ripple } from "primereact/ripple";
+import { StyleClass } from "primereact/styleclass";
 
+import IconBtn from "./Icon";
+import { AppSidebarOptions } from "@/types/SidebarComponent";
+import Styles from "./Styles.module.scss";
+import { ChevronDown } from "lucide-react";
 
+const OptionsComponent = ({
+  sidebarOpenState,
+  menuList,
+  sidebarSelected,
+}: any) => {
+  const pathname = usePathname();
+  const route = pathname.split("/")[1];
+  const btnRef3 = useRef(null);
 
-import IconBtn from './Icon';
-import { AppSidebarOptions } from '@/types/SidebarComponent';
-import Styles from './Styles.module.scss'
-import { ChevronDown } from 'lucide-react';
-
-const OptionsComponent = ({ sidebarOpenState, menuList, sidebarSelected }: any) => {
-
-    const pathname = usePathname();
-    const route = pathname.split('/')[1]
-    const btnRef3 = useRef(null);
-
-    return (
-        <div className={`${sidebarOpenState
-            ? `${Styles.sidebar_menu_container_open}`
-            : `${Styles.sidebar_menu_container_close}`
-            } ${Styles.sidebar_menu_container}`}
-        >
-            {menuList?.map((option: AppSidebarOptions, index: Key) => {
-                return (
-                    <div key={index} >
-                        <div
-                            className={` ${!sidebarOpenState ? `${Styles.sidebar_menu}` : `${Styles.sidebar_menu_open}`} ${sidebarSelected === option.link && "surface-200"} ${sidebarOpenState ? `${Styles.menulist_open}` : `${Styles.menulist_close}`}`}
-                        >
-                            <IconBtn
-                                alt={option.tabName}
-                                height={option.size}
-                                width={option.size}
-                                iconName={option.iconName}
-                                enableIcons={sidebarSelected === option.link}
-                                activeOnHover={true}
-                                icon={option.icon}
-                                link={option.link}
-                                route={route}
-                            />
-                            {sidebarOpenState && (
-                                <div className={`${Styles.menulist_text} ${route === option.link ? `${Styles.menulist_text_active}` : ''}`}>
-                                    {
-                                        option?.options && option?.options?.length > 0 ? (
-                                            <div>
-                                               <StyleClass nodeRef={btnRef3} selector=".tab" enterClassName="hidden" leaveToClassName="hidden">
-                                                    <a ref={btnRef3} className='flex gap-2'>
-                                                        <span className="font-medium">Revenue</span>
-                                                        <ChevronDown style={{ color: 'gray' }} size={20}/>
-                                                        <Ripple />
-                                                    </a>
-                                                </StyleClass>
-                                                <ul className="list-none tab w-full ml-[-1.2rem] hidden overflow-y-hidden transition-all transition-duration-400 transition-ease-in-out">
-                                                    <li className='flex gap-2 p-3'>
-                                                    {/* <homeSetting className={Styles.nestedIcon}/> */}
-                                                        <a className=" flex align-items-center cursor-pointer  hover:surface-100 transition-duration-150 transition-colors w-full">
-                                                            <span className="font-medium">View</span>
-                                                        </a>
-                                                    </li>
-                                                    <li  className='flex gap-2 p-3'>
-                                                    {/* <option.icon className={Styles.nestedIcon}/> */}
-                                                        <a className=" flex align-items-center cursor-pointer hover:surface-100 transition-duration-150 transition-colors w-full">
-                                                            <span className="font-medium">Search</span>
-                                                        </a>
-                                                    </li>
-                                                </ul>
-                                            </div>
-                                        ) : (
-                                            <Link href={option.link}>
-                                                <p>
-                                                    {option?.tabName}
-                                                </p>
-                                            </Link>
-                                        )
-                                    }
-                                </div>
-                            )}
-                        </div>
-                        {/* <div className={`${Styles.nested_sidebar_components} expand hidden`}>
+  return (
+    <div
+      className={`${
+        sidebarOpenState
+          ? `${Styles.sidebar_menu_container_open}`
+          : `${Styles.sidebar_menu_container_close}`
+      } ${Styles.sidebar_menu_container}`}
+    >
+      {menuList?.map((option: AppSidebarOptions, index: Key) => {
+        return (
+          <div key={index}>
+            <div
+              className={` ${
+                !sidebarOpenState
+                  ? `${Styles.sidebar_menu}`
+                  : `${Styles.sidebar_menu_open}`
+              } ${sidebarSelected === option.link && "surface-200"} ${
+                sidebarOpenState
+                  ? `${Styles.menulist_open}`
+                  : `${Styles.menulist_close}`
+              }`}
+            >
+              <IconBtn
+                alt={option.tabName}
+                height={option.size}
+                width={option.size}
+                iconName={option.iconName}
+                enableIcons={sidebarSelected === option.link}
+                activeOnHover={true}
+                icon={option.icon}
+                link={option.link}
+                route={route}
+              />
+              {sidebarOpenState && (
+                <div
+                  className={`${Styles.menulist_text} ${
+                    route === option.link
+                      ? `${Styles.menulist_text_active}`
+                      : ""
+                  }`}
+                >
+                  {option?.options && option?.options?.length > 0 ? (
+                    <div>
+                      <StyleClass
+                        nodeRef={btnRef3}
+                        selector=".tab"
+                        enterClassName="hidden"
+                        leaveToClassName="hidden"
+                      >
+                        <a ref={btnRef3} className="flex gap-2">
+                          <span className="font-medium">Revenue</span>
+                          <ChevronDown style={{ color: "gray" }} size={20} />
+                          <Ripple />
+                        </a>
+                      </StyleClass>
+                      <ul className="list-none tab w-full ml-[-1.2rem] hidden overflow-y-hidden transition-all transition-duration-400 transition-ease-in-out">
+                        <li className="flex gap-2 p-3">
+                          {/* <homeSetting className={Styles.nestedIcon}/> */}
+                          <a className=" flex align-items-center cursor-pointer  hover:surface-100 transition-duration-150 transition-colors w-full">
+                            <span className="font-medium">View</span>
+                          </a>
+                        </li>
+                        <li className="flex gap-2 p-3">
+                          {/* <option.icon className={Styles.nestedIcon}/> */}
+                          <a className=" flex align-items-center cursor-pointer hover:surface-100 transition-duration-150 transition-colors w-full">
+                            <span className="font-medium">Search</span>
+                          </a>
+                        </li>
+                      </ul>
+                    </div>
+                  ) : (
+                    <Link href={option.link}>
+                      <p>{option?.tabName}</p>
+                    </Link>
+                  )}
+                </div>
+              )}
+            </div>
+            {/* <div className={`${Styles.nested_sidebar_components} expand hidden`}>
                             {option?.options && option?.options?.length > 0 && sidebarOpenState && (
                                 <div className={`${Styles.submenu_container}`} >
                                     {option?.options?.map((subOption: AppSidebarOptions, index: Key) => {
@@ -111,11 +129,11 @@ const OptionsComponent = ({ sidebarOpenState, menuList, sidebarSelected }: any) 
                                 </div>
                             )}
                         </div> */}
-                    </div>
-                );
-            })}
-        </div>
-    )
-}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
-export default OptionsComponent
+export default OptionsComponent;
