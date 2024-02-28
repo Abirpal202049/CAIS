ddb6ff3d715c57b130f135e9f9d6ac23130b0775
Niraj Modi <modiniraj1034@gmail.com>
2024-02-28 23:21:18 +0530

    column names changed in all alert type

--- src/app/(dashboard)/admin/alert-type-config/page.tsx
+++ src/app/(dashboard)/admin/alert-type-config/page.tsx
@@ -28,11 +28,54 @@ export default function AlertTypePage({}: Props) {
         return <div>{data[field]}</div>;
     }
   };
+
+  const showColumns = [
+    {
+      field: "name",
+      header: "Name",
+    },
+    {
+      field: "identifier",
+      header: "Identifier",
+    },
+    {
+      field: "action_name_array",
+      header: "Actions",
+    },
+    {
+      field: "action_id_array",
+      header: "Actions Count",
+    },
+    {
+      field: "display_name_array",
+      header: "Display",
+    },
+    {
+      field: "display_id_array",
+      header: "Display Count",
+    },
+    {
+      field: "workflow_name_array",
+      header: "Workflow",
+    },
+    {
+      field: "workflow_id_array",
+      header: "Workflow Count",
+    },
+    {
+      field: "configured",
+      header: "Configured",
+    },
+  ];
   return (
     <div>
       <h2 className="!text-2xl">All Alert Type</h2>
       {!isLoading && (
-        <Custom_Table data={data?.message} handleSwitch={handleSwitch} />
+        <Custom_Table
+          data={data?.message}
+          handleSwitch={handleSwitch}
+          showColumns={showColumns}
+        />
       )}
     </div>
   );
