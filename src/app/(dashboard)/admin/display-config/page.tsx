81421b485ac16bb100194c70501a11b15484000e
mehraankush <mehraankush36@gmail.com>
2024-03-01 12:01:07 +0530

    integrate action post

--- src/app/(dashboard)/admin/display-config/page.tsx
+++ src/app/(dashboard)/admin/display-config/page.tsx
@@ -1,10 +1,19 @@
 "use client"
 import React, { useEffect, useState, useMemo } from 'react'
-import { useGetDisplay } from '../_api/alert_config';
 import Custom_Table from '@/components/common/Custom_Table';
+import { Plus } from 'lucide-react';
+import { InputText } from 'primereact/inputtext';
+import { Button } from 'primereact/button';
+import { Dialog } from 'primereact/dialog';
+import { useCreateDisplayType, useGetDisplay } from '../_api/display.config';
 
 const Page = () => {
   const { data , isLoading, isError } = useGetDisplay();
+  const [visible, setVisible] = useState(false);
+  const [inputData, setInputData] = useState({
+    name:''
+  })
+  const { mutate, isPending } = useCreateDisplayType()
 
   const handleSwitch = (data: any, field: any) => {
     switch (field) {
@@ -32,8 +41,35 @@ const Page = () => {
     },
   ];
 
+  const handleDisplaySubmit = () => {
+    mutate(inputData, {
+      onSuccess: (data) => {
+        console.log('Success ', data)
+      },
+      onError: (err) => {
+        console.log('Success ', err)
+      },
+      onSettled: (data, error) => {
+        console.log('Success ', data, error)
+      },
+    })
+    if (!isPending) {
+      setVisible(false);
+      setInputData({
+        name:''
+      })
+    }
+  }
+
   return (
     <div className='overflow-hidden'>
+      <div className=' flex justify-between px-3'>
+        <p className='!text-xl font-semibold'>Action Configuration</p>
+        <div className='flex gap-1 cursor-pointer justify-center items-center text-brand font-bold hover:border-primary' onClick={() => setVisible(!visible)}>
+          <Plus/> <p>Add Action</p>
+        </div>
+      </div>
+      <div>
        {!isLoading && (
         <Custom_Table 
          data={data?.data} 
@@ -41,6 +77,19 @@ const Page = () => {
          showColumns={showColumns}
         />
       )}
+      </div>
+      <Dialog header="Add New Action" visible={visible} style={{ width: '30vw' }} onHide={() => setVisible(false)} className='rounded p-2'>
+        <div className="flex flex-col  items-center">
+          <div className='w-4/5 flex flex-col gap-5'>
+            <div className='flex flex-col gap-2'>
+              <label htmlFor="rname">Name</label>
+              <InputText id="rname" aria-describedby="username-help" onChange={(e)=>setInputData({name:e.target.value})} value={inputData.name}/>
+            </div>
+
+            <Button label='Create Action' onClick={handleDisplaySubmit} disabled={isPending} />
+          </div>
+        </div>
+      </Dialog>
     </div>
   )
 }
